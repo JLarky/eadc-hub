a5551878e99a6d8e3da64dcc0246064a4833b156
JLarky <jlarky@gmail.com>
2009-02-20 23:19:31 +0300

    Добавил утилитки для работы с base32

--- src/eadc_utils.erl
+++ src/eadc_utils.erl
@@ -2,6 +2,7 @@
 -author('jlarky@gmail.com').
 
 -export([parse/2, reverse/1]).
+-export([random_base32/1, base32/1]).
 
 parse(simple, String) ->
     parse(simple, [], [], String).
@@ -19,3 +20,20 @@ parse(simple, Out, Buf, [ Elem | Tail ]) ->
 reverse(List)             -> reverse([], List).
 reverse(Out, [])          -> Out;
 reverse(Out, [Elem|Tail]) -> reverse([Elem|Out], Tail).
+
+
+random_base32(Count) ->
+    random_base32(Count, []).
+random_base32(0, Output) ->
+    Output;
+random_base32(Count, Output) ->
+    random_base32(Count-1, Output)++base32(random:uniform(32)-1).
+
+
+base32(V) ->
+    if
+	V < 0 -> error;
+	V < 26 -> [V+65];
+	V > 25 -> [V+24]; % V-26+48+2
+	true -> V
+    end.
