bae90a109081adeb19cb584221aa307e1549a2f0
JLarky <jlarky@gmail.com>
2009-03-05 01:40:10 +0300

    Now hub can do !help command

--- src/plugin_bot.erl
+++ src/plugin_bot.erl
@@ -4,6 +4,7 @@
 	chat_msg/1]).
 
 -include("eadc.hrl").
+-include("eadc_plugin.hrl").
 
 user_login(Args) ->
     {value,{pid,Pid}} = lists:keysearch(pid, 1, Args),
@@ -15,7 +16,25 @@ user_login(Args) ->
 
 chat_msg(Args) ->
     ?DEBUG(debug, "chat_msg: ~w~n", [Args]),
-    %%{value,{pid,Pid}} = lists:keysearch(pid, 1, Args),
-    %%{string, String1} = eadc_utils:convert({args, ["BMSG", "AAAA", "echo"]}),
-    %%gen_fsm:send_event(Pid, {send_to_socket, String1}).
-    Args.
+    ?GET_VAL(msg, Msg),
+    case Msg of 
+	[$!|Command] -> %% command
+	    do_command(Command),
+	    lists:keyreplace(pids, 1, Args, {pids, []});
+	_ -> 
+	    Args
+    end.
+
+do_command(Command) ->
+    case Command of
+	"help" ->
+	    Hlp="All hub commands:
+!help - for show this help",
+	    eadc_utils:info_to_pid(self(), Hlp);
+	_ ->
+	    eadc_utils:info_to_pid(self(), "неопознаная комманда =)"),
+	    Out=Command,
+	    Test=lists:flatten(io_lib:format("~w", [Out])),
+	    eadc_utils:error_to_pid(self(), Test)
+    end.
+
