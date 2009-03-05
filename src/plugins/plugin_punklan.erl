8137daede9a3349fb379aedeb28e9123e8bfce5f
JLarky <jlarky@gmail.com>
2009-03-05 14:47:56 +0300

    Посылаем переменную стейт того чела к кому коннектимся

--- src/plugins/plugin_punklan.erl
+++ src/plugins/plugin_punklan.erl
@@ -13,13 +13,14 @@
 -export([ctm/1]). %% some hook that you want to catch
 
 ctm(Args) ->
-    %% ?GET_VAL(msg, Msg), %% macro for extracting 'msg' param from 'Args'
+    ?GET_VAL(pids, [Pid]), %% macro for extracting 'msg' param from 'Args'
 
     %% send message to sender
     eadc_utils:info_to_pid(self(), "Ты только что попробовал приконнектится"),
 
     %% if we want see Erlang term in chat we have do like that
-    Out=Args, 
+    %%Out='gen_fsm:sync_send_all_state_event(self(), get_state, 1000)',
+    Out=gen_fsm:sync_send_all_state_event(Pid, get_state),
     Test=lists:flatten(io_lib:format("~w", [Out])),
     eadc_utils:error_to_pid(self(), Test),
 
