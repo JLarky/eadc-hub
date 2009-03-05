9a42197f2e9d57471c5009222a818190649897f1
JLarky <jlarky@gmail.com>
2009-03-05 13:46:53 +0300

    Начнём писать плагин для убивания внешних коннектов

---
+++ src/plugins/plugin_punklan.erl
@@ -0,0 +1,27 @@
+%%%-------------------------------------------------------------------
+%%% Author  : JLarky <jlarky@jlarky.info>
+%%% Description : Some plugin for something
+%%%
+%%% Created :  
+%%%-------------------------------------------------------------------
+-module(plugin_punklan).
+-author('jlarky@gmail.com').
+
+-include("eadc.hrl").
+-include("eadc_plugin.hrl").
+
+-export([ctm/1]). %% some hook that you want to catch
+
+ctm(Args) ->
+    %% ?GET_VAL(msg, Msg), %% macro for extracting 'msg' param from 'Args'
+
+    %% send message to sender
+    eadc_utils:info_to_pid(self(), "Ты только что попробовал приконнектится"),
+
+    %% if we want see Erlang term in chat we have do like that
+    %%Out=[1,2,3,{1,2,3}], 
+    %%Test=lists:flatten(io_lib:format("~w", [Out])),
+    %%eadc_utils:error_to_pid(self(), Test),
+
+    %% YOU ALWAYS MAST DO THAT
+    Args.
