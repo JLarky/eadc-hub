a171d7bff2a6b7c91c04d483386a0d6a7e43d94b
JLarky <jlarky@gmail.com>
2009-03-05 12:17:59 +0300

    first working inplementation of plugin_ten_lines

--- src/plugin_ten_lines.erl
+++ src/plugin_ten_lines.erl
@@ -10,11 +10,11 @@
 -include("eadc.hrl").
 -include("eadc_plugin.hrl").
 
--export([chat_msg/1, user_login/1]).
+-export([chat_msg/1, user_login/1, init/1]).
 
 -define(ETS_CHECK, case ets:info(plugin_ten_lines) of
 		       undefined ->
-			   ets:new(plugin_ten_lines, [bag, named_table, public]);
+			   ets:new(plugin_ten_lines, [set, named_table, public]);
 		       _ -> ok
 		   end).
 
@@ -23,16 +23,27 @@ chat_msg(Args) ->
     ?ETS_CHECK,
     ?GET_VAL(msg, Msg), ?GET_VAL(sid, Sid),
     ?GET_VAL(pid, _Pid), ?GET_VAL(nick, Nick),
-    ets:insert(plugin_ten_lines, {time(), Sid, Msg, Nick}),
+    [{ten_lines, [_|Tail]=Msgs}]= ets:lookup(plugin_ten_lines, ten_lines),
+    if length(Msgs) > 10 ->
+	    Old_Msgs=Tail;
+       true ->
+	    Old_Msgs=Msgs
+    end,
+    New_Msgs=Old_Msgs++[{time(), Sid, eadc_utils:unquote(Msg), Nick}],
+    ets:insert(plugin_ten_lines, {ten_lines, New_Msgs}),
     Args.
 
 user_login(Args) ->
     ?GET_VAL(pid, Pid),
     ?DEBUG(debug, "~w: user_login ~w", [?MODULE, Pid]),
     ?ETS_CHECK,
-    Msgs = 
-    ets:foldl(fun({_Time, _Sid, Msg, Nick}, Acc) ->
-		      lists:concat([Acc, "-", Nick, " писал: ", Msg, "\n"])
-	      end, "Последние сообщения хаба:\n", plugin_ten_lines),
-    eadc_utils:send_to_pid(Pid, {args, ["ISTA", "000", Msgs]}),
+    [{ten_lines, Msgs}]= ets:lookup(plugin_ten_lines, ten_lines),
+    Ten_lines = lists:foldl(fun({_Time, _Sid, Msg, Nick}, Acc) ->
+				    lists:concat([Acc, "-", Nick, " писал: ", Msg, "\n"])
+			    end, "Последние сообщения хаба:\n", Msgs),
+    eadc_utils:info_to_pid(self(), Ten_lines),
+    Args.
+
+init(Args) ->
+    ?ETS_CHECK,
     Args.
