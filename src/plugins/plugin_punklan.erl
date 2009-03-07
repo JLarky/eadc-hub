97f86de275ac7acf9e930ff13776203bdce0fd91
JLarky <jlarky@gmail.com>
2009-03-07 21:10:58 +0300

    Бот банит "внешние" соединения и умеет выдавать из какой человек общаги по комманде ".ip"

--- src/plugins/plugin_punklan.erl
+++ src/plugins/plugin_punklan.erl
@@ -14,21 +14,44 @@
 -export([ctm/1, chat_msg/1]). %% some hook that you want to catch
  
 chat_msg(Args) ->
-    %%?GET_VAL(pids, Pids),
-    %%?GET_VAL(state, State),
-    %%eadc_utils:info_to_pid(self(), io_lib:format("Кастую! ~s",[whois(State#state.addr)])),
-    Args.
+    ?GET_VAL(msg, Msg),
+    case Msg of
+	[$., $i, $p | Tail] ->
+	    case Tail of
+		_ ->
+		    ?GET_VAL(state, State),
+		    #state{nick=Nick, addr=Addr}=State,
+		    Place=whois(Addr)
+	    end,
+	    Place_m =  case Place of
+			   inet -> "интернетах. Осторожно! трафик!";
+			   _    -> f("~w общаге.",[Place])
+		       end,
+	    eadc_utils:info_to_pid(self(), f("Пользователь ~s находится в ~s", [Nick, Place_m])),
+	    lists:keyreplace(pids, 1, Args, {pids, []});
+	_ ->
+	    Args
+    end.
  
 ctm(Args) ->
     ?GET_VAL(pids, [Pid]=_Pids),
     ?GET_VAL(state, State_f),
-    State_t=gen_fsm:sync_send_all_state_event(Pid, get_state),
+    State_t=gen_fsm:sync_send_all_state_event(Pid, get_state, 10000),
     Nick_f=State_f#state.nick,
     Nick_t=State_t#state.nick,
     Direction=lists:concat([whois(State_f#state.addr), "\\sи\\s", whois(State_t#state.addr)]),
  
     %% send message to sender
-    eadc_utils:info_to_pid(self(), io_lib:format("~s только что попробовал приконнектится к ~s. (~s)",[Nick_f, Nick_t,Direction])),
+    eadc_utils:info_to_pid(self(), io_lib:format("~s только что попробовал приконнектиться к ~s. (~s)",[Nick_f, Nick_t,Direction])),
+    case {whois(State_f#state.addr), whois(State_t#state.addr)} of
+	{F, T} when (F == inet) or (T == inet) ->
+	    eadc_utils:info_to_pid(self(), "Но нифига из этого не выйдет."),
+	    lists:keyreplace(pids, 1, Args, {pids, []});
+	_ ->
+	    Args
+    end.
+    
+
  
     %% if we want see Erlang term in chat we have do like that
     %%Out='gen_fsm:sync_send_all_state_event(self(), get_state, 1000)',
@@ -41,8 +64,12 @@ ctm(Args) ->
     %%eadc_utils:error_to_pid(self(), Test),
  
     %% YOU ALWAYS MAST DO THAT
-    Args.
+    %%Args.
  
  
 whois({N1, N2, N3, N4}) ->
     ?GET_CAMPUS_NAME(N4+256*(N3+256*(N2+256*N1))).
+
+
+f(F, A) ->
+    lists:flatten(io_lib:format(F, A)).
