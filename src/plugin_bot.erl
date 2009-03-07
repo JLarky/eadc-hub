0500557b656c631b8c2910b83779022a852e528a
JLarky <jlarky@gmail.com>
2009-03-08 01:18:08 +0300

    nothing changed

--- src/plugin_bot.erl
+++ src/plugin_bot.erl
@@ -9,7 +9,7 @@
 user_login(Args) ->
     {value,{pid,Pid}} = lists:keysearch(pid, 1, Args),
     {string, String1} = eadc_utils:convert({args, ["IINF", "CT32", "VEJLarky's hub", "NIADC-hub", "DEБот"]}),
-    {string, String2} = eadc_utils:convert({args, ["ISTA", "000", "Добро пожаловать в ADC-хаб написанный на Erlang. Страничка проекта http://wiki.github.com/JLarky/eadc-hub"]}),
+    {string, String2} = eadc_utils:convert({args, ["ISTA", "000", "Добро пожаловать в ADC-хаб написанный на Erlang. Страничка проекта http://wiki.github.com/JLarky/eadc-hub на ней можно узнать что такое ADC и почему именно Erlang."]}),
     gen_fsm:send_event(Pid, {send_to_socket, String1}),
     gen_fsm:send_event(Pid, {send_to_socket, String2}),
     Args.
