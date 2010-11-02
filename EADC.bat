0d9dd3ad7aa2890551b158173bde70fc85954a49
JLarky <jlarky@gmail.com>
2010-11-02 14:46:29 +0300

    Still playing with rebar.

--- EADC.bat
+++ EADC.bat
@@ -1,1 +1,3 @@
-cd ebin && erl -pa ..\deps\*\ebin -sname eadc -boot eadc
+cd ebin &&
+mkdir -p ../priv && cp ../deps/tiger/priv/*.dll ../priv/ &&
+erl -pa ../deps/*/ebin/ -boot start_sasl -sname eadc -eval "application:start(tiger)" -eval "application:start(eadc)" $@
